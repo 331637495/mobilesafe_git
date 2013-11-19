6c8331cc78112d4987018977ed7b5502549f370d
331637495 <331637495@qq.com>
2013-11-19 17:11:55 +0800

    sinianxiugai

--- mobilesafe/src/com/xawx/mobilesafe/MainActivity.java
+++ mobilesafe/src/com/xawx/mobilesafe/MainActivity.java
@@ -13,6 +13,9 @@ public class MainActivity extends Activity {
 		
 	}
 
+	public void getname(){
+	}
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.

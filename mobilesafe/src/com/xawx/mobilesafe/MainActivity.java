a87dc5d68cbe470ef8b9699857aae0a1236fce54
331637495 <331637495@qq.com>
2013-11-19 14:57:10 +0800

    sinian

--- mobilesafe/src/com/xawx/mobilesafe/MainActivity.java
+++ mobilesafe/src/com/xawx/mobilesafe/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		System.out.println("hello word");
+		System.out.println("nihao");
 	}
 
 	@Override

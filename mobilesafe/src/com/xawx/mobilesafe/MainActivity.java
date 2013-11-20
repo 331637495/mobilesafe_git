48855e423b4c31df61ef813b320b8368902c2606
331637495 <331637495@qq.com>
2013-11-20 16:01:46 +0800

    sinianxiugai

--- mobilesafe/src/com/xawx/mobilesafe/MainActivity.java
+++ mobilesafe/src/com/xawx/mobilesafe/MainActivity.java
@@ -12,9 +12,7 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		
 	}
-
-	System.out.println("Œ“ «TortoiseGit");
-
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
